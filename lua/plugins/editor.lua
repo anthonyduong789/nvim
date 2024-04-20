6f58e658e0d4613fcdc7c624bdd84e4debeaf4b8
Anthony Duong <anthonyduong789@gmail.com>
2024-04-20 13:13:36 -0700

    changed some key bindings

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -89,6 +89,28 @@ return {
         end,
         desc = "Open File Browser with the path of the current buffer",
       },
+      {
+        ";ls",
+        function()
+          local telescope = require("telescope")
+
+          local function telescope_buffer_dir()
+            return vim.fn.expand("%:p:h")
+          end
+
+          telescope.extensions.file_browser.file_browser({
+            path = "%:p:h",
+            cwd = telescope_buffer_dir(),
+            respect_gitignore = false,
+            hidden = true,
+            grouped = true,
+            previewer = false,
+            initial_mode = "normal",
+            layout_config = { height = 40 },
+          })
+        end,
+        desc = "Open File Browser with the path of the current buffer",
+      },
     },
     config = function(_, opts)
       local telescope = require("telescope")
